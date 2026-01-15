fc88aac295f3a64472f67d07fd4c74d24fbf1fd2
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-01-15 12:20:56 +0800

    added string to enum conversion blank files

--- src/nuclide_reaction_and_decay_data/mod.rs
+++ src/nuclide_reaction_and_decay_data/mod.rs
@@ -1,3 +1,11 @@
-pub struct NuclideReactionAndDecayData {
+use fission_yields_data::prelude::Nuclide;
 
+#[derive(Debug, PartialEq)]
+pub struct NuclideReactionAndDecayData {
+    // contains the nuclide of interest
+    pub nuclide: Nuclide,
 }
+
+/// First, to convert the nuclide name, I want to convert it to an enum
+/// this will require long match statements
+pub mod name_string_to_enum_conversion;
