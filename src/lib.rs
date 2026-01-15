590454f8514957ee0498fab3ba0d350c15e815ab
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-01-15 11:54:16 +0800

    added a nuclide reaction and decay data

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,7 @@ pub use fission_yields_data::prelude::Nuclide::*;
 /// this contains the raw information 
 /// based on pwr neutron spectrum
 pub mod decay_xml_info_serde;
+
+/// this is the struct that converts the SerdeNuclideData to 
+/// NuclideReactionAndDecayData 
+pub mod nuclide_reaction_and_decay_data;
