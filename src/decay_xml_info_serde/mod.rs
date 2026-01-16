ce85bed582fa051f69df0a4ac5af5702349c6713
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2026-01-16 15:00:08 +0800

    added electronvolt to decay energy

--- src/decay_xml_info_serde/mod.rs
+++ src/decay_xml_info_serde/mod.rs
@@ -206,7 +206,7 @@ pub struct SerdeNuclideData {
     #[serde(rename = "@decay_modes")]
     decay_modes: Option<u32>,
     #[serde(rename = "@decay_energy")]
-    decay_energy: Option<f64>,
+    decay_energy_electronvolt: Option<f64>,
     #[serde(rename = "@reactions")]
     reactions: Option<u32>,
 
