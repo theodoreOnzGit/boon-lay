788c11fc3f478bc9c13144c3750d8c8642fdc3a6
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2026-01-21 11:03:35 +0800

    included glob imports for serde only in prelude

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -1,3 +1,4 @@
+pub use crate::decay_xml_info_serde;
 pub use crate::decay_xml_info_serde::*;
 pub use crate::nuclide_reaction_and_decay_data::HalfLifeAndDecayEnergyInfo;
 pub use crate::nuclide_reaction_and_decay_data::NuclideReactionAndDecayData;
