37fc2edd5ebd9ce22b1a1bb390f7dac487af082a
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-01-22 10:25:28 +0800

    changed citation disclaimer and acknowledgements

--- examples/boon_lay_decay_simulator/decay_simulator_v1/panels_and_pages/citation_disclaimer_and_acknowledgements.rs
+++ examples/boon_lay_decay_simulator/decay_simulator_v1/panels_and_pages/citation_disclaimer_and_acknowledgements.rs
@@ -11,12 +11,6 @@ impl DecaySimApp {
         ui.label(" ");
 
         ui.label("This is an educational simulator under testing and development");
-        ui.label("Limited Validation has been done on transient forced circulation");
-        ui.label("and steady statenatural circulation");
-        ui.label("Validation is still work in progress");
-        ui.label("This is given under GPLv3 without ANY WARRANY");
-        ui.label("Results are not guaranteed to be physically accurate");
-        ui.label("USE AT YOUR OWN RISK");
 
         ui.label(" ");
         ui.label(" ");
@@ -33,7 +27,7 @@ impl DecaySimApp {
         
         ui.label(" ");
 
-        ui.label("Heater, cooler and heat exchanger artwork from DWSIM released under GPLv3");
+        ui.label("Note: Some code was vibe coded with help from ChatGPT5, esp boilerplate code");
 
         ui.label(" ");
         ui.label(" ");
@@ -41,25 +35,10 @@ impl DecaySimApp {
         ui.heading("Citations appreciated:");
         ui.label(" ");
         
-        ui.label("@phdthesis{ong2024digital,");
-        ui.label("title={Digital Twins as Testbeds for Iterative Simulated Neutronics Feedback Controller Development},");
-        ui.label("author={Ong, Theodore Kay Chen},");
-        ui.label("year={2024},");
-        ui.label("school={UC Berkeley}");
-        ui.label("}");
+        ui.label("TBC");
 
         ui.label(" ");
 
-        ui.label("@article{ong2024open,");
-        ui.label("title={An open-source Thermo-hydraulic Uniphase Advection and Convection Solver for Salt Flows (TUAS)},");
-        ui.label("author={Ong, Theodore Kay Chen and Xiao, Sicong and Peterson, Per F},");
-        ui.label("journal={International Journal of Advanced Nuclear Reactor Design and Technology},");
-        ui.label("volume={6},");
-        ui.label("number={4},");
-        ui.label("pages={281--301},");
-        ui.label("year={2024},");
-        ui.label("publisher={Elsevier}");
-        ui.label("}");
     }
 
 }
