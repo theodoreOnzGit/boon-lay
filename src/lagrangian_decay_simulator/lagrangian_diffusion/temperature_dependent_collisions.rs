5a3db1d7a2867c50290db935a2ce6cbaf09a4316
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-02-02 16:28:11 +0800

    q1 and d1 for cs okay

--- src/lagrangian_decay_simulator/lagrangian_diffusion/temperature_dependent_collisions.rs
+++ src/lagrangian_decay_simulator/lagrangian_diffusion/temperature_dependent_collisions.rs
@@ -1,9 +1,11 @@
 use std::f64::consts::PI;
 
-use uom::si::f64::{
-    Energy, Length, Mass, ThermodynamicTemperature, Time, Velocity,
-};
+use fission_yields_data::prelude::Nuclide;
+use uom::si::areal_number_density::per_square_meter;
+use uom::si::diffusion_coefficient::square_meter_per_second;
+use uom::si::f64::*;
 use uom::si::energy::joule;
+use uom::si::molar_energy::kilojoule_per_mole;
 use uom::si::ratio::ratio;
 use uom::si::thermodynamic_temperature::kelvin;
 
@@ -52,6 +54,139 @@ pub fn expected_collisions_atomic_jumps(
 
     return (t/mean_free_path * v_mean).get::<ratio>();
 }
+
+
+// diffusion coefficient 
+// from Jiang 2023
+// Jiang, W., Toptan, A., Hales, J. D., Spencer, B. W., & 
+// Novascone, S. R. (2023). Fission product transport in TRISO particles 
+// and pebbles (No. INL/EXT-21-63549-Rev001). Idaho National Lab.(INL), 
+// Idaho Falls, ID (United States).
+//
+// D = D1 exp (-Q1/RT) + D2 exp (-Q2/RT)
+pub fn diffusion_coeff_jiang(
+    triso_layer: TrisoLayerMaterial,
+    nuclide: Nuclide,
+    temperature: ThermodynamicTemperature,
+    ) -> Option<DiffusionCoefficient> {
+
+    let (z,_a) = nuclide.get_z_a();
+
+    let d1: Option<DiffusionCoefficient> = match z {
+        // Silver 
+        _ => None
+    };
+
+    todo!()
+
+}
+
+// from Jiang 2023
+// Jiang, W., Toptan, A., Hales, J. D., Spencer, B. W., & 
+// Novascone, S. R. (2023). Fission product transport in TRISO particles 
+// and pebbles (No. INL/EXT-21-63549-Rev001). Idaho National Lab.(INL), 
+// Idaho Falls, ID (United States).
+//
+// table on page 13 of 105
+#[inline]
+pub fn get_d1_for_ag(triso_layer: TrisoLayerMaterial,) -> DiffusionCoefficient{
+
+    let coeff_m2_per_s: f64 = match triso_layer {
+        TrisoLayerMaterial::Kernel => 6.7e-9,
+        TrisoLayerMaterial::PyC => 5.3e-9,
+        TrisoLayerMaterial::SiC => 3.6e-9,
+    };
+
+    return DiffusionCoefficient::new::<square_meter_per_second>(
+        coeff_m2_per_s
+    );
+
+
+}
+
+// from Jiang 2023
+// Jiang, W., Toptan, A., Hales, J. D., Spencer, B. W., & 
+// Novascone, S. R. (2023). Fission product transport in TRISO particles 
+// and pebbles (No. INL/EXT-21-63549-Rev001). Idaho National Lab.(INL), 
+// Idaho Falls, ID (United States).
+//
+// table on page 13 of 105
+#[inline]
+pub fn get_q1_for_ag(triso_layer: TrisoLayerMaterial,) -> MolarEnergy {
+
+    let coeff_kj_per_mol: f64 = match triso_layer {
+        TrisoLayerMaterial::Kernel => 165.0,
+        TrisoLayerMaterial::PyC => 154.0,
+        TrisoLayerMaterial::SiC => 215.0,
+    };
+
+    return MolarEnergy::new::<kilojoule_per_mole>(coeff_kj_per_mol);
+
+
+}
+
+// from Jiang 2023
+// Jiang, W., Toptan, A., Hales, J. D., Spencer, B. W., & 
+// Novascone, S. R. (2023). Fission product transport in TRISO particles 
+// and pebbles (No. INL/EXT-21-63549-Rev001). Idaho National Lab.(INL), 
+// Idaho Falls, ID (United States).
+//
+// table on page 13 of 105
+#[inline]
+pub fn get_d1_for_cs(triso_layer: TrisoLayerMaterial,
+    gamma_neutron_fluence: ArealNumberDensity) -> DiffusionCoefficient{
+
+    let coeff_m2_per_s: f64 = match triso_layer {
+        TrisoLayerMaterial::Kernel => 5.6e-8,
+        TrisoLayerMaterial::PyC => 6.3e-8,
+        TrisoLayerMaterial::SiC => {
+            let gamma_neutron_fluence_neutrons_per_sqm = 
+                gamma_neutron_fluence.get::<per_square_meter>();
+
+            let exponential_factor = 
+                (gamma_neutron_fluence_neutrons_per_sqm * 1.1/5.0).exp();
+
+            5.5e-14 * exponential_factor
+        },
+    };
+
+    return DiffusionCoefficient::new::<square_meter_per_second>(
+        coeff_m2_per_s
+    );
+
+
+}
+
+// from Jiang 2023
+// Jiang, W., Toptan, A., Hales, J. D., Spencer, B. W., & 
+// Novascone, S. R. (2023). Fission product transport in TRISO particles 
+// and pebbles (No. INL/EXT-21-63549-Rev001). Idaho National Lab.(INL), 
+// Idaho Falls, ID (United States).
+//
+// table on page 13 of 105
+#[inline]
+pub fn get_q1_for_cs(triso_layer: TrisoLayerMaterial,) -> MolarEnergy {
+
+    let coeff_kj_per_mol: f64 = match triso_layer {
+        TrisoLayerMaterial::Kernel => 209.0,
+        TrisoLayerMaterial::PyC => 222.0,
+        TrisoLayerMaterial::SiC => 125.0,
+    };
+
+    return MolarEnergy::new::<kilojoule_per_mole>(coeff_kj_per_mol);
+
+
+}
+
+
+/// triso layer for diffusion
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum TrisoLayerMaterial {
+    Kernel,
+    PyC,
+    SiC,
+}
+
 
 
 
