3b34806b4ddce2110f942fbdba9659ece5f68a8b
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-02-11 15:40:20 +0800

    added sphere

--- src/lagrangian_decay_simulator/lagrangian_diffusion/single_particle_simulator/constructive_solid_geometry/mod.rs
+++ src/lagrangian_decay_simulator/lagrangian_diffusion/single_particle_simulator/constructive_solid_geometry/mod.rs
@@ -16,44 +16,6 @@ pub enum Region {
     Plane,
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub(crate) struct Sphere {
-    x: Length,
-    y: Length,
-    z: Length,
-    r: Length
-}
+pub(crate) mod sphere;
+pub(crate) use sphere::*;
 
-impl Sphere {
-    fn is_point_in_sphere(&self, point: [Length; 3]) -> bool {
-
-        let center_meters: [f64;3] = [
-            self.x.get::<meter>(),
-            self.y.get::<meter>(),
-            self.z.get::<meter>(),
-        ];
-
-        let point_meters: [f64;3] = [
-            point[0].get::<meter>(),
-            point[1].get::<meter>(),
-            point[2].get::<meter>(),
-        ];
-        let radius_meters = self.r.get::<meter>();
-
-        point_in_sphere(center_meters, radius_meters, point_meters)
-
-    }
-}
-
-/// Returns `true` if point `p` is inside or on the sphere, `false` otherwise.
-/// Sphere is defined by `center` and `radius`.
-#[inline]
-fn point_in_sphere(center: [f64; 3], radius: f64, p: [f64; 3]) -> bool {
-    let dx = p[0] - center[0];
-    let dy = p[1] - center[1];
-    let dz = p[2] - center[2];
-
-    // Compare squared distance to squared radius (avoids sqrt, faster)
-    let dist2 = dx.mul_add(dx, dy.mul_add(dy, dz * dz));
-    dist2 <= radius * radius
-}
