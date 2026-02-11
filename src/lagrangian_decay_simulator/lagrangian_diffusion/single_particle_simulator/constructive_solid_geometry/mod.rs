55ccdb2072b5111e1951a0e899358b16c0501acc
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-02-11 15:28:32 +0800

    added stuff on norms

--- src/lagrangian_decay_simulator/lagrangian_diffusion/single_particle_simulator/constructive_solid_geometry/mod.rs
+++ src/lagrangian_decay_simulator/lagrangian_diffusion/single_particle_simulator/constructive_solid_geometry/mod.rs
@@ -1,2 +1,25 @@
 // the thing about constructive_solid_geometry (CSG) 
 // is detemrine where a particle is relative to a shape or plane.
+//
+// for the sphere, the L2 norm (straight line distance) will suffice 
+// as to 
+
+
+pub mod norms;
+pub use norms::*;
+use uom::si::f64::*;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Region {
+    Sphere(Sphere),
+    Box,
+    Plane,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub(crate) struct Sphere {
+    x: Length,
+    y: Length,
+    z: Length,
+    r: Length
+}
