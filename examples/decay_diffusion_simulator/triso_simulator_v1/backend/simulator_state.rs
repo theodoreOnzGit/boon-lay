15cff9f595b25eed8dac73a9dac292b3cfa10d6c
teddy0@arch-desktop-aftershock <theodore_ong@berkeley.edu>
2026-02-13 08:42:09 +0800

    reduced timestep for cs-137

--- examples/decay_diffusion_simulator/triso_simulator_v1/backend/simulator_state.rs
+++ examples/decay_diffusion_simulator/triso_simulator_v1/backend/simulator_state.rs
@@ -30,7 +30,7 @@ impl Default for SimulatorState {
         let is_running = true;
         let restart_button_pressed = false;
         let change_nuclide_button_pressed = false;
-        let user_selected_timestep = Time::new::<second>(70.0);
+        let user_selected_timestep = Time::new::<second>(10.0);
         let elapsed_time = Time::ZERO;
         let simulated_time = Time::ZERO;
 
