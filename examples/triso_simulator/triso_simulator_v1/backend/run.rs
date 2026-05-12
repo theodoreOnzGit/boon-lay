ef8fa7b134514378ea31c5d3ef9aaf4a432e7e27
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2026-05-12 13:54:54 +0800

    thread sleeps even less now

--- examples/triso_simulator/triso_simulator_v1/backend/run.rs
+++ examples/triso_simulator/triso_simulator_v1/backend/run.rs
@@ -264,7 +264,7 @@ impl TRISOSimApp {
                 thread::sleep(time_to_sleep_realtime);
             } else {
                 let time_to_sleep_milliseconds: u64 = 
-                    20;
+                    5;
                 let time_to_sleep_non_realtime: Duration = 
                     Duration::from_millis(time_to_sleep_milliseconds);
                 thread::sleep(time_to_sleep_non_realtime);
